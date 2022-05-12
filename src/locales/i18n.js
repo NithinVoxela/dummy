ee8df404d8443916cceb611bd4844f396d63749b
Avinash T <tavinashb@gmail.com>
2022-05-12 22:57:11 +0530

    i18n fix

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import XHR from "i18next-http-backend";
 //
 import enLocales from './en.json';
 import jaLocales from './ja.json';
@@ -8,10 +9,12 @@ import jaLocales from './ja.json';
 // ----------------------------------------------------------------------
 
 const options = {
-  order: ['navigator']  
+  order: ['querystring', 'navigator'],
+  lookupQuerystring: 'lng'  
 }
 
 i18n
+  .use(XHR)
   .use(LanguageDetector)  
   .use(initReactI18next)
   .init({
@@ -20,8 +23,7 @@ i18n
       en: { translations: enLocales },  
       ja: { translations: jaLocales }
     },
-    order: ['navigator', 'querystring', 'cookie', 'localStorage', 'sessionStorage', 'htmlTag', 'path', 'subdomain'],
-    lng: localStorage.getItem('i18nextLng') || 'en',
+    order: ['navigator', 'querystring', 'cookie', 'localStorage', 'sessionStorage', 'htmlTag', 'path', 'subdomain'],    
     fallbackLng: 'en',
     debug: false,
     ns: ['translations'],
